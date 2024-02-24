623a0fd55c6b94bb4731409e75f3febad1888050
Zahid Hussain <96944633+zahid3519@users.noreply.github.com>
2024-02-24 12:10:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create object
+const softwareCompany = {
+    name: "The Smart Tech",
+    founder: "Zahid",
+    yearFounded: 2002,
+    products: [".Net", "Reack js", "Node js"]
+};
+
+// Access  properties to the console
+console.log("Company name:", softwareCompany.name);
+console.log("Founder:", softwareCompany.founder);
+console.log("Year founded:", softwareCompany.yearFounded);
+console.log("Products:", softwareCompany.products);
